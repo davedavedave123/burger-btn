f0f6f5ff78f5b22c2fc93f53c718a3780aaebcaa
davedavedave123 <44082128+davedavedave123@users.noreply.github.com>
2019-12-21 07:21:59 +1300

    Create btn.js

---
+++ btn.js
@@ -0,0 +1,26 @@
+let burger = document.getElementById("burger-btn");
+burger.onclick = burgerClick;
+let burgerIsClosed = true;
+
+function burgerClick(){
+  let btop = document.getElementById("burger-top");
+	let bbot = document.getElementById("burger-bot");
+
+	if ( burgerIsClosed ) {
+	    
+        btop.style.transform = "rotate(45deg) translateY(10px)";
+        bbot.style.transform = "rotate(-45deg) translateY(-10px)";
+        bbot.style.width = "40px";
+
+        burgerIsClosed = false;
+
+	} else {
+
+	    btop.style.transform = "rotate(0deg) translateY(0px)";
+        bbot.style.transform = "rotate(0deg) translateY(0px)";
+        bbot.style.width = "23px";
+
+        burgerIsClosed = true;
+	
+	}	
+}
