051d0499eac291d30334309aa3d37c8d689987e5
davedavedave123 <44082128+davedavedave123@users.noreply.github.com>
2019-12-21 07:24:12 +1300

    added comments

--- btn.js
+++ btn.js
@@ -1,24 +1,25 @@
+// Burger Menu Animation
 let burger = document.getElementById("burger-btn");
 burger.onclick = burgerClick;
 let burgerIsClosed = true;
 
 function burgerClick(){
-  let btop = document.getElementById("burger-top");
+	let btop = document.getElementById("burger-top");
 	let bbot = document.getElementById("burger-bot");
 
 	if ( burgerIsClosed ) {
 	    
-        btop.style.transform = "rotate(45deg) translateY(10px)";
-        bbot.style.transform = "rotate(-45deg) translateY(-10px)";
-        bbot.style.width = "40px";
+		btop.style.transform = "rotate(45deg) translateY(10px)";
+		bbot.style.transform = "rotate(-45deg) translateY(-10px)";
+		bbot.style.width = "40px";
 
         burgerIsClosed = false;
 
 	} else {
 
-	    btop.style.transform = "rotate(0deg) translateY(0px)";
-        bbot.style.transform = "rotate(0deg) translateY(0px)";
-        bbot.style.width = "23px";
+		btop.style.transform = "rotate(0deg) translateY(0px)";
+		bbot.style.transform = "rotate(0deg) translateY(0px)";
+		bbot.style.width = "23px";
 
         burgerIsClosed = true;
 	
